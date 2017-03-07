3e00767f054b9cdeb71720a2874f7bd8e67a364d
Jaynarol <jaynarol@gmail.com>
2017-03-07 13:04:33 +0700

    add Validate Test

--- test/Utils/ValidateTest.js
+++ test/Utils/ValidateTest.js
@@ -1,3 +1,65 @@
-/**
- * Created by Jaynarol on 007 07-Mar-17.
- */
+/* eslint-disable prefer-arrow-callback */
+
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import Validate from '../../src/Utils/Validate'
+
+describe('Validate', function () {
+  describe('checkNumber()', function () {
+    it('input Number should be throw nothing', function () {
+      return expect(function () {
+        Validate.checkNumber(10)
+      })
+        .to.not.throw()
+    })
+
+    it('input Double should be throw nothing', function () {
+      return expect(function () {
+        Validate.checkNumber(10.99)
+      })
+        .to.not.throw()
+    })
+
+    it('input Number < 0 should be throw', function () {
+      return expect(function () {
+        Validate.checkNumber(-10)
+      })
+        .to.throw(/invalid damage/)
+    })
+
+    it('input null should be throw', function () {
+      return expect(function () {
+        Validate.checkNumber(null)
+      })
+        .to.throw(/invalid damage/)
+    })
+
+    it('input NaN should be throw', function () {
+      return expect(function () {
+        Validate.checkNumber(NaN)
+      })
+        .to.throw(/invalid damage/)
+    })
+
+    it('input String should be throw', function () {
+      return expect(function () {
+        Validate.checkNumber('10')
+      })
+        .to.throw(/invalid damage/)
+    })
+
+    it('input undefined should be throw', function () {
+      return expect(function () {
+        Validate.checkNumber(undefined)
+      })
+        .to.throw(/invalid damage/)
+    })
+
+    it('input Infinity should be throw', function () {
+      return expect(function () {
+        Validate.checkNumber(Infinity)
+      })
+        .to.throw(/invalid damage/)
+    })
+  })
+})
